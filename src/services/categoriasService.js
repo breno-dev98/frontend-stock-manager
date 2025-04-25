7c9f5b47bc147c56f044975817a7fa32557c5e90
Breno Oliveira <brenosoader1@gmail.com>
2025-04-25 15:36:06 -0300

    Criando services de categorias

---
+++ src/services/categoriasService.js
@@ -0,0 +1,43 @@
+import api from "../api/index";
+
+export class CategoriaService {
+    static async getAll() {
+        try {
+            const response = await api.get("/categorias");
+            return response.data;
+        } catch (error) {
+            console.error("Erro ao buscar categorias:", error);
+            throw error; // Re-lança o erro para que o componente possa lidar com ele, caso necessário
+        }
+    }
+
+    static async create(data) {
+        try {
+            const response = await api.post("/categorias", data);
+            return response.data.categorias;
+        } catch (error) {
+            console.error("Erro ao criar categoria:", error);
+            throw error;
+        }
+    }
+
+    static async update(id, data) {
+        try {
+            const response = await api.put(`/categorias/${id}`, data);
+            return response.data.categoria;
+        } catch (error) {
+            console.error(`Erro ao atualizar categoria com id ${id}:`, error);
+            throw error;
+        }
+    }
+
+    static async delete(id) {
+        try {
+            const response = await api.delete(`/categorias/${id}`);
+            return response.data;
+        } catch (error) {
+            console.error(`Erro ao deletar categoria com id ${id}:`, error);
+            throw error;
+        }
+    }
+}
