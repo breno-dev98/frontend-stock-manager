1115d368312baff25e92ced823a9aa7a4472324f
Breno Oliveira <brenosoader1@gmail.com>
2025-05-15 21:37:39 -0300

    criando teste para acessar a rota de marcas através do menu da sidebar

---
+++ cypress/e2e/marcas.cy.js
@@ -0,0 +1,11 @@
+describe('Acesso à página de marcas', () => {
+    beforeEach(() => {
+        cy.login(); // faz login antes de cada teste
+    });
+
+    it('Deve navegar para marcas via sidebar', () => {
+        cy.get('#marcas_nav').click();
+        cy.url().should('include', '/marcas');
+        cy.contains('Produtos').should('exist');
+    });
+});
